c9ff0425682de4d62ae516894f1ac52f770f5002
polakovap <123546884+polakovap@users.noreply.github.com>
2024-05-11 22:13:52 +0100

    .

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,27 +1,33 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
 
 const loginUser = async (req, res) => {
     const { username, password } = req.body;
 
-  try {
-    // Check if the user exists in the database
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
     }
 
-    // Validate the password
-    if (user.password !== password) {
-        return res.status(401).json({ message: "Invalid password" });
-    }
+    try {
+        // Check if the user exists in the database
+        const user = await User.findOne({ username });
 
-    // Authentication successful
-    res.status(200).json({ message: "Login successful", user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-}
+        if (!user) {
+        return res.status(404).json({ message: "User not found" });
+        }
+
+        // Compare the hashed password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+
+        // Authentication successful
+        res.status(200).json({ message: "Login successful", user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+ }
 
 module.exports = {
     loginUser
--- controllers/registration.controller.js
+++ controllers/registration.controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user.model');
 
 const registerUser = async (req, res) => {
@@ -10,8 +11,11 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
+    //hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // Create a new user record
-    const newUser = new User({ username, email, password });
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ message: "User registered successfully" });
